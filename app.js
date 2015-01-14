dd7b3a351b3ac24db8ec642255aefaef11d88d59
Subtirelu Mihail <mihail.subtirelu@kalon.ro>
2015-01-14 17:11:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var play = require('play');
+
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//Set the port of the application
+app.set('port', (process.env.PORT || 5000));
+
+//Set the public folder
+app.use(express.static(__dirname + '/public'));
+
+//Set up the body parser for the RestfullApi
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: false
+}));
+
+//Homepage
+app.get('/', function(req, res) {
+  res.render('pages/index',{
+    alarm: {
+      minutes: 59,
+      hour: 23
+    }
+  });
+});
+
+//RestfulAPI
+app.get('/sound', function(req, res) {
+   play.sound('./public/sound/alarm.wav');
+   response.end();
+});
+
+//Starting to listen...
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
+
